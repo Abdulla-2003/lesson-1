00f29166293cf212ce909a92cb8cd94f3d6ed3d8
Abdulla-2003 <baki272003@gmail.com>
2025-05-31 20:32:10 +0300

    Hello

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num = 567;
+        int number = num % 10 * 100;
+        num /= 10;
+        number += num % 10 * 10;
+        num /= 10;
+        number += num;
+
+        System.out.println(number);
+
+    }
+}
